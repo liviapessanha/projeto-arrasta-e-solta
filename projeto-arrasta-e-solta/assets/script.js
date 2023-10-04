24d29744fda1c33376c0941b1589aaf5c5fee2b4
Lívia Pessanha <105252356+liviapessanha@users.noreply.github.com>
2023-10-04 10:45:49 -0300

    Add files via upload

---
+++ projeto-arrasta-e-solta/assets/script.js
@@ -0,0 +1,85 @@
+let areas = {
+  a: null,
+  b: null,
+  c: null,
+  d: null,
+  e: null
+};
+
+
+document.querySelectorAll('.item').forEach(item => {
+  item.addEventListener('dragstart', dragStart);
+  item.addEventListener('dragend', dragEnd);
+});
+
+document.querySelectorAll('.area').forEach(area => {
+  area.addEventListener('dragover', dragOver);
+  area.addEventListener('dragleave', dragLeave);
+  area.addEventListener('drop', drop);
+});
+
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral);
+
+function dragStart(e) {
+  e.currentTarget.classList.add('dragging');
+} 
+function dragEnd(e) {
+  e.currentTarget.classList.remove('dragging');
+}
+
+function dragOver(e) {
+  if(e.currentTarget.querySelector('.item') === null) {
+      e.preventDefault();
+     e.currentTarget.classList.add('hover');
+  }
+}
+function dragLeave(e) {
+  e.currentTarget.classList.remove('hover');
+}
+function drop(e) {
+  e.currentTarget.classList.remove('hover');
+
+  let dragItem = document.querySelector('.item.dragging');
+  let areaName = e.currentTarget.getAttribute('data-name');
+  let classeItem = dragItem.classList[1];
+
+ if( verificaFrase(areaName, classeItem)) {
+    if(!dragItem.classList.contains('cloned')) {
+      let clonedItem = dragItem.cloneNode(true);
+      clonedItem.classList.add('cloned');
+      document.querySelector('.neutralArea').appendChild(clonedItem);
+    }
+
+  e.currentTarget.appendChild(dragItem);
+ }
+}
+ 
+
+function dragOverNeutral(e) {
+  e.preventDefault();
+   e.currentTarget.classList.add('hover');
+}
+function dragLeaveNeutral(e) {
+e.currentTarget.classList.remove('hover');
+}
+function dropNeutral(e) {
+e.currentTarget.classList.remove('hover');
+let dragItem = document.querySelector('.item.dragging');
+e.currentTarget.appendChild(dragItem);
+}
+
+function verificaFrase(area, classeItem) {
+  const frasesPermitidas = {
+    a:['frase1', 'frase2'],
+    b:['frase3', 'frase4'],
+    c:['frase5', 'frase6'],
+  };
+
+  if(frasesPermitidas[area].includes(classeItem)) {
+    return true;
+  } else {
+    return false;
+  }
+}
